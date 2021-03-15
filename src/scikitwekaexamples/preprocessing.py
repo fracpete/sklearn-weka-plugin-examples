6850b15208d865ca2e5bf7ff918881376e1d5d45
Peter Reutemann <fracpete@gmail.com>
2021-03-15 17:00:48 +1300

    transforming class now as well

--- src/scikitwekaexamples/preprocessing.py
+++ src/scikitwekaexamples/preprocessing.py
@@ -21,8 +21,15 @@ def main():
     tr = WekaTransformer(classname="weka.filters.unsupervised.attribute.Standardize")
     print(tr.to_commandline())
     X_new, y_new = tr.fit(X, y).transform(X, y)
-    print(X_new)
-    print(y_new)
+    print("transformed X:\n", X_new)
+    print("same y:\n", y_new)
+
+    helper.print_info("Standardizing data (incl class)")
+    tr = WekaTransformer(classname="weka.filters.unsupervised.attribute.Standardize", options=["-unset-class-temporarily"])
+    print(tr.to_commandline())
+    X_new, y_new = tr.fit(X, y).transform(X, y)
+    print("transformed X:\n", X_new)
+    print("transformed y:\n", y_new)
 
 
 if __name__ == "__main__":
