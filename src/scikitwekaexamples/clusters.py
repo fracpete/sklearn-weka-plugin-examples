5bd73805baddc3f10f17573a10fb5f977aa92ccd
Peter Reutemann <fracpete@gmail.com>
2021-03-16 09:28:33 +1300

    added clustering

---
+++ src/scikitwekaexamples/clusters.py
@@ -0,0 +1,35 @@
+import os
+import scikit.weka.jvm as jvm
+import scikitwekaexamples.helper as helper
+import traceback
+
+from scikit.weka.clusters import WekaCluster
+from scikit.weka.dataset import load_arff
+
+
+def main():
+    """
+    Just runs some example code.
+    """
+
+    iris_file = helper.get_data_dir() + os.sep + "iris.arff"
+    helper.print_info("Loading dataset: " + iris_file)
+    X, y, meta = load_arff(iris_file, "last")
+
+    helper.print_info("Standardizing data (incl class)")
+    cl = WekaCluster(classname="weka.clusterers.SimpleKMeans", options=["-N", "3"])
+    print(cl.to_commandline())
+    clusters = cl.fit_predict(X)
+    print("class label -> cluster label")
+    for i in range(len(y)):
+        print(y[i], "->", clusters[i])
+
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception as e:
+        print(traceback.format_exc())
+    finally:
+        jvm.stop()
