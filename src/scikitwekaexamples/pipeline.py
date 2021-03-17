1013b9965d41f95e5013e8fb4443472865d940f2
Peter Reutemann <fracpete@gmail.com>
2021-03-17 14:05:58 +1300

    added a mixed pipeline

--- src/scikitwekaexamples/pipeline.py
+++ src/scikitwekaexamples/pipeline.py
@@ -7,7 +7,8 @@ from scikit.weka.classifiers import WekaEstimator
 from scikit.weka.preprocessing import WekaTransformer
 from scikit.weka.dataset import load_arff
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 def main():
@@ -19,7 +20,20 @@ def main():
     helper.print_info("Loading dataset: " + bolts_file)
     X, y, meta = load_arff(bolts_file, "last")
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+    # Weka pipeline
+    helper.print_info("Weka pipeline")
     std = WekaTransformer(classname="weka.filters.unsupervised.attribute.Standardize")
+    lr = WekaEstimator(classname="weka.classifiers.functions.LinearRegression", options=["-R", "1.0e-8"])
+    pipe = make_pipeline(std, lr)
+    print("Pipeline:\n", pipe)
+    pipe.fit(X_train, y_train)  # apply standardization on training data
+    score = pipe.score(X_test, y_test)  # apply standardization on testing data, without leaking training data.
+    print("score:", score)
+
+    # Mixed pipeline
+    helper.print_info("Mixed pipeline")
+    std = StandardScaler()
     lr = WekaEstimator(classname="weka.classifiers.functions.LinearRegression", options=["-R", "1.0e-8"])
     pipe = make_pipeline(std, lr)
     print("Pipeline:\n", pipe)
