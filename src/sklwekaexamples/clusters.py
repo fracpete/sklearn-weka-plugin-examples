191650a0d8720e00bce27110e3b7c7048fb828af
Peter Reutemann <fracpete@gmail.com>
2022-03-31 17:35:37 +1300

    added nominal_input_vars/nominal_output_var to WekaEstimator and nominal_input_vars to WekaCluster to avoid manual conversion of data

--- src/sklwekaexamples/clusters.py
+++ src/sklwekaexamples/clusters.py
@@ -12,13 +12,28 @@ def main():
     Just runs some example code.
     """
 
+    # iris
     iris_file = helper.get_data_dir() + os.sep + "iris.arff"
     helper.print_info("Loading dataset: " + iris_file)
     X, y, meta = load_arff(iris_file, class_index="last")
 
-    helper.print_info("Simple K-Means")
+    helper.print_info("Simple K-Means (iris)")
     cl = WekaCluster(classname="weka.clusterers.SimpleKMeans", options=["-N", "3"])
     print(cl.to_commandline())
+    clusters = cl.fit_predict(X)
+    print("class label -> cluster label")
+    for i in range(len(y)):
+        print(y[i], "->", clusters[i])
+
+    # vote
+    vote_file = helper.get_data_dir() + os.sep + "vote.arff"
+    helper.print_info("Loading dataset: " + vote_file)
+    X, y, meta = load_arff(vote_file, class_index="last")
+
+    helper.print_info("Simple K-Means (vote)")
+    cl = WekaCluster(classname="weka.clusterers.SimpleKMeans", options=["-N", "3"], nominal_input_vars=[x for x in range(X.shape[1])])
+    print(cl.to_commandline())
+    print(repr(cl))
     clusters = cl.fit_predict(X)
     print("class label -> cluster label")
     for i in range(len(y)):
